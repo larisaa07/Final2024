3f8dcd770c33aa6527385274dc2a7810673174a4
larisaa07 <larisa_aveti@yahoo.com>
2024-06-05 13:44:10 -0400

    gave up on parallax; just need to style+add imgs to aboutUs pg

--- script.js
+++
@@ -1,7 +0,0 @@
-//parallax scroll:
-document.addEventListener('scroll', function(){
-    let scrollPosition = window.scrollY;
-
-    document.querySelector('.parallax-layer:nth-child(1)').style.transform = 'translateY(' + (-scrollPosition * 0.5) + 'px)';
-    document.querySelector('.parallax-layer:nth-child(2)').style.transform = 'translateY(' + (-scrollPosition * 0.8) + 'px)';
-})
